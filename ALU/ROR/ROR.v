cf84d1c9dae2c8da76ca6fedba96f4460519ca9c
PsaoFlamand <72673573+PsaoFlamand@users.noreply.github.com>
2020-11-04 12:04:23 -0800

    Update ROR.v

--- ALU/ROR/ROR.v
+++ ALU/ROR/ROR.v
@@ -1,6 +1,14 @@
-module ROR(A, N, Result);
-input [31:0] A;
-input [3:0] N;
+module ROR(In1, In2, Result,Flag,S,New_Flag);	//[N, Z, C, V}]
+input [31:0] In1;
+input [3:0] In2;
+input S;
+input [3:0] Flag;
 output [31:0] Result;
-assign Result = (A >> N) | (A << (32-N));
+output [3:0] New_Flag;
+wire Carry;
+
+assign Result = (In1 >> In2) | (In1 << (32-In2));
+
+//Flag assignments
+SET_FLAG(In1, In2, Result, Flag, S, New_Flag, Carry)
 endmodule
