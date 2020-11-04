8ecea0b581b36780a77fb195ff0b0861c95e7f73
PsaoFlamand <72673573+PsaoFlamand@users.noreply.github.com>
2020-11-04 12:03:41 -0800

    Update AND.v

--- ALU/AND/AND.v
+++ ALU/AND/AND.v
@@ -1,5 +1,12 @@
-module AND(A, B, Result);
-input [31:0] A, B;
+module AND(In1, In2, Result,Flag,S,New_Flag);	//[N, Z, C, V}]
+input [31:0] In1, In2;
+input S;
+input [3:0] Flag;
 output [31:0] Result;
-assign Result = A&B;
+output [3:0] New_Flag;
+wire Carry;
+assign {Carry,Result} = A&B;
+
+//Flag assignments
+SET_FLAG(In1, In2, Result,Flag,S,New_Flag,Carry)
 endmodule
