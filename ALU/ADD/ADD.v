d33992bc87622e132b7f98c873c59de5554332cf
PsaoFlamand <72673573+PsaoFlamand@users.noreply.github.com>
2020-11-04 12:01:53 -0800

    Update ADD.v

--- ALU/ADD/ADD.v
+++ ALU/ADD/ADD.v
@@ -1,11 +1,11 @@
-module ADD(A, B, Result, Flag, S);	//flags [N, Z, C, V]
-input [31:0] A, B;
+module ADD(In1, In2, Result,Flag,S,New_Flag);	//[N, Z, C, V}]
+input [31:0] In1, In2;
+input S;
+input [3:0] Flag;
 output [31:0] Result;
-output [3:0] Flag;
+output [3:0] New_Flag;
 wire Carry;
-assign {Carry,Result} = A + B;
-Set_Flag S1(Carry,S);
-//assign Flag[1]=S?C:0; //Set flags if Sbit is 1
-//assign Flag[3:2]=0;
-//assign Flag[0]=0;
+assign {Carry,Result} = In1 + In2;
+//Flag assignments
+SET_FLAG(In1, In2, Result,Flag,S,New_Flag,Carry)
 endmodule
