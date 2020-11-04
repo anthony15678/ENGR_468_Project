c32a4fd47dc0e25787469117d559c25e2dee1bc4
PsaoFlamand <72673573+PsaoFlamand@users.noreply.github.com>
2020-11-04 12:02:52 -0800

    Update LSR.v

--- ALU/LSR/LSR.v
+++ ALU/LSR/LSR.v
@@ -1,6 +1,16 @@
-module LSR(A, N, Result);
-input [31:0] A;
-input [3:0] N;
+module LSR(In1, In2, Result,Flag,S,New_Flag);	//[N, Z, C, V}]
+parameter n = 1;
+input [31:0] In1
+input [n-1:0], In2;
+input S;
+input [3:0] Flag;
+
 output [31:0] Result;
-assign Result = A>>N;
+output [3:0] New_Flag;
+wire Carry;
+assign {Carry,Result} = In1>>In2;
+
+//Flag assignments
+SET_FLAG(In1, In2, Result,Flag,S,New_Flag,Carry)
+
 endmodule
