cd9bb07fc2688d9fdc7ad4e8a57ef0f30fb50aa7
caffeine98 <49334026+caffeine98@users.noreply.github.com>
2020-11-04 18:26:56 -0800

    update sub.v 1

--- ALU/SUB/SUB.v
+++ ALU/SUB/SUB.v
@@ -1,8 +1,15 @@
-module SUB(x, y, out);
+module SUB(In1, In2, Result, Flag, S, New_Flag);
 
-input [31:0] x,y;
-output [31:0] out;
+input signed [31:0] In1, In2;
+input [3:0] Flag;
+input S;
+output signed [31:0] Result;
+wire Carry;
 
-assign out = a - b;
+assign Result In1 - In2;
+//assign Result = (In1[31]) ? ~In1 + 1b'1 + In2 : (In2[31]) ? In1 + ~In2 + 1b'1 : 0;
+//assign Result = (In1[31]) ? ~In1 + 1b'1 : (In2[31]) ? ~In2 + 1b'1;
+
+SET_FLAG set(In1, In2, Result, Flag, S, New_Flag, Carry);
 
 endmodule
