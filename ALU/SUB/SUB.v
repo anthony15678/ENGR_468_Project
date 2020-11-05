a8431614b2b536f661b7e89469a9c8be9a397017
caffeine98 <49334026+caffeine98@users.noreply.github.com>
2020-11-04 18:32:41 -0800

    update sub.v 2

--- ALU/SUB/SUB.v
+++ ALU/SUB/SUB.v
@@ -6,7 +6,7 @@ input S;
 output signed [31:0] Result;
 wire Carry;
 
-assign Result In1 - In2;
+assign {Carry,Result} = In1 - In2;
 //assign Result = (In1[31]) ? ~In1 + 1b'1 + In2 : (In2[31]) ? In1 + ~In2 + 1b'1 : 0;
 //assign Result = (In1[31]) ? ~In1 + 1b'1 : (In2[31]) ? ~In2 + 1b'1;
 
